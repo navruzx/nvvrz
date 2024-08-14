ca0c822383506513b1792e14d60fc22030bba799
navruzx <nnavruz095@gmail.com>
2024-08-14 17:44:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+let getByAllClass = document.querySelectorAll("#div")
+function changeColor(color) {
+   switch (color) {
+    case 0:
+        getByAllClass  [0].style.cssText = `background:black;`;
+        getByAllClass [0].innerHTML = "0"
+        break;
+        case 1:
+            getByAllClass [1].style.cssText = `background:black;`;
+            getByAllClass [1].innerHTML = "0"
+            break;
+            case 2:
+                getByAllClass [2].style.cssText = `background:black;`;
+                getByAllClass [2].innerHTML = "0"
+                break;
+                case 3:
+                    getByAllClass [3].style.cssText = `background:black;`;
+                    getByAllClass [3].innerHTML = "0"
+                    break;
+                    case 4:
+                        getByAllClass [4].style.cssText = `background:black;`;
+                        getByAllClass [4].innerHTML = "0"
+                        break
+                        case 5:
+                            getByAllClass [5].style.cssText = `background:black;`;
+                            getByAllClass [5].innerHTML = "0"
+                            break
+                            case 6:
+                                getByAllClass [6].style.cssText = `background:black;`;
+                                getByAllClass [6].innerHTML = "0"
+                                break
+                                case 7:
+                                    getByAllClass [7].style.cssText = `background:black;`;
+                                    getByAllClass [7].innerHTML = "0"
+                                    break
+                                    case 8:
+                                        getByAllClass [8].style.cssText = `background:black;`;
+                                        getByAllClass [8].innerHTML = "0"
+                                        break
+
+                                    
+   }
+}
+changeColor()
